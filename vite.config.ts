3fb3d3f29697e1b4f6b57fcf0b1b9dae3b1aaa42
dibhell <ptr@o2.pl>
2025-12-04 14:09:16 +0100

    chore: set test base path /visus-tst/ for tst branch

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/visus/',   // <--- NAZWA REPO
-  //base: './', // Important for GitHub Pages relative paths
+  // Dedykowany prefix dla środowiska testowego (Gałąź tst -> GitHub Pages /visus-tst/)
+  base: '/visus-tst/',
 })
