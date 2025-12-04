2b894a0e8b900d354c76cc4bf59d5f97c2afdf4b
dibhell <ptr@o2.pl>
2025-12-04 14:42:42 +0100

    Deploy visus-tst build

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // Dedykowany prefix dla środowiska testowego (Gałąź tst -> GitHub Pages /visus-tst/)
-  base: '/visus-tst/',
-})
